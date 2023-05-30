aea2d3bb0f81f9d08816dc5148921225dc53f7f7
sfshovon <shahriar.shovon@northsouth.edu>
2023-05-30 18:03:34 +0600

    Updated UI Of NotFound

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -8,11 +8,11 @@ const NotFound = () => {
       <PageTitle title="Not Found"/>
       <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
         <div className="max-w-md text-center">
-          <h2 className="mb-8 font-extrabold text-9xl">
-            <span className="sr-only">Error</span>404
+          <h2 className="mb-8 font-extrabold text-3xl md:text-7xl">
+            Error <span className="text-red-700">404</span> 
           </h2>
           <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
-          <Link rel="noopener noreferrer" to="/" className="mt-10 btn px-8 py-3 font-semibold rounded">Back to homepage</Link>
+          <Link to="/" className="mt-10 btn px-8 py-3 font-semibold rounded">Back to homepage</Link>
         </div>
       </div>
     </section>
